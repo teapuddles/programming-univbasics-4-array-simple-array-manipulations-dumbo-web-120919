3d3a006c27a44fcfd69a8f65ef51b35c796e12f8
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-26 16:43:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,32 @@ end
 def pop_with_args(array)
   array.pop(-1,-2)
 end
+
+def using_unshift(array)
+  array.unshift 
+end
+
+def shift_with_args(array)
+  
+end
+
+def using_concat(array)
+  
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at(array, integer)
+  
+end
+
