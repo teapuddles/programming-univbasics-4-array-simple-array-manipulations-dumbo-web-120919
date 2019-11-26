773227f742b57b81cc16ddc025e3c15b20013cfa
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-26 16:59:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,7 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer)
-  array.delete(integer)
+def using_delete_at(array, 2)
+  array.delete_at(2)
 end
 
