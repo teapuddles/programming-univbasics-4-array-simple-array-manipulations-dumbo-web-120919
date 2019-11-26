34906a48a6901e241c02a52a0a93ea3b7990e7ec
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-26 16:52:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,11 +7,11 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  array.pop 
+  array.pop
 end
 
 def pop_with_args(array)
-  array.pop(-1,-2)
+  array.pop(2)
 end
 
 def using_shift(array)
@@ -19,26 +19,30 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  
+  array.shift(2)
 end
 
-def using_concat(array)
-  
+def using_concat(array, array1)
+  array.concat(array1)
+end
+
+def using_insert(array, element)
+  array.insert(element)
 end
 
 def using_uniq(array)
-  
+  array.uniq
 end
 
 def using_flatten(array)
-  
+  array.flatten
 end
 
 def using_delete(array, string)
-  
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
-  
+  array.delete(integer)
 end
 
